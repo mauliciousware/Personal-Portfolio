7c205eef01c1f7b277e7643517a4c284b8f72564
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-10-13 19:09:19 -0400

    update resume

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -27,7 +27,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "https://drive.google.com/file/d/1evVM75HwskoBCvoSEnSZoZ31XxBkW48p/view?usp=sharing", icon: NotebookIcon, label: "Resume" },
+    { href: "https://drive.google.com/file/d/1pmB2v4Bylp4iE3uLVHEuEoZUIaKu6CKa/view?usp=sharing", icon: NotebookIcon, label: "Resume" },
   ],
   contact: {
     email: "sraut2@binghamton.edu",
