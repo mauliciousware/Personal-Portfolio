862af019d07c02a916931a8a22c1291b66d22ebb
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-09-12 00:49:16 -0400

    Update resume link in navbar

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -27,7 +27,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "https://drive.google.com/file/d/12Ushv5whp40FCSwQNAke6wb6cqj2_msw/view?usp=sharing", icon: NotebookIcon, label: "Resume" },
+    { href: "https://drive.google.com/file/d/1r_oL5U6COtV8LiV5FzJEPw-w5XtKqGLo/view?usp=sharing", icon: NotebookIcon, label: "Resume" },
   ],
   contact: {
     email: "sraut2@binghamton.edu",
